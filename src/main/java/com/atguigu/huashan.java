0e131f46c581f5ef5f2ced7a1cea0a34c4a30c73
xinyuanfan <xinyunfan88@163.com>
2020-12-14 20:08:10 +0800

    updata 1

--- src/main/java/com/atguigu/huashan.java
+++ src/main/java/com/atguigu/huashan.java
@@ -3,5 +3,10 @@ package com.atguigu;
 public class huashan {
     public static void main(String[] args) {
         System.out.println("辟邪剑谱");
+        System.out.println("葵花宝典");
+        System.out.println("独孤九剑");
+        System.out.println("紫霞神功");
+        System.out.println("吸星大法");
+
     }
 }
